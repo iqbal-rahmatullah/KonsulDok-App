4f87bb69a796ba3b6be8af7d9c59b92c564427a7
iqbal-rahmatullah <iqbalrahmmatullah01@gmail.com>
2024-05-03 23:18:59 +0700

    Navbar widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:konsul_dok/pages/chat_page.dart';
 import 'package:konsul_dok/pages/home_page.dart';
+import 'package:konsul_dok/pages/jadwal_page.dart';
 import 'package:konsul_dok/pages/login_page.dart';
+import 'package:konsul_dok/pages/profile_page.dart';
 import 'package:konsul_dok/pages/register_page.dart';
+import 'package:konsul_dok/utils/color.dart';
+import 'package:konsul_dok/widgets/navbar.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  var selectedIndex = 0;
+  var page = [
+    const HomePage(),
+    const JadwalPage(),
+    const ChatPage(),
+    const ProfilePage(),
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      color: Color(0xffffffff),
+      color: MyColor.putih,
       home: Scaffold(
-        body: HomePage(),
+        body: page[selectedIndex],
+        bottomNavigationBar: MyBottomNavigationBar(
+          selectedIndex: selectedIndex,
+          onItemTapped: (index) {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+        ),
       ),
     );
   }
---
+++ lib/widgets/navbar.dart
@@ -0,0 +1,81 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import 'package:konsul_dok/utils/color.dart';
+import 'package:konsul_dok/utils/textstyle.dart';
+
+class MyBottomNavigationBar extends StatefulWidget {
+  final int selectedIndex;
+  final Function onItemTapped;
+  const MyBottomNavigationBar({
+    Key? key,
+    required this.selectedIndex,
+    required this.onItemTapped,
+  }) : super(key: key);
+
+  @override
+  State<MyBottomNavigationBar> createState() => _MyBottomNavigationBarState();
+}
+
+class _MyBottomNavigationBarState extends State<MyBottomNavigationBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Theme(
+      data: ThemeData(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Container(
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                blurRadius: 10,
+                offset: const Offset(0, 5),
+              ),
+            ],
+          ),
+          child: ClipRRect(
+            borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+            child: BottomNavigationBar(
+              backgroundColor: MyColor.putih,
+              type: BottomNavigationBarType.fixed,
+              unselectedLabelStyle:
+                  MyTextStyle.deskripsi.copyWith(color: MyColor.abuText),
+              showUnselectedLabels: true,
+              unselectedItemColor: MyColor.abuText,
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.home_outlined),
+                  label: 'Beranda',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.calendar_today),
+                  label: 'Jadwal',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.chat_bubble_outline),
+                  label: 'Chat',
+                ),
+                BottomNavigationBarItem(
+                  icon: Icon(Icons.person_2_outlined),
+                  label: 'Profil',
+                ),
+              ],
+              selectedLabelStyle:
+                  MyTextStyle.deskripsi.copyWith(color: MyColor.biru),
+              currentIndex: widget.selectedIndex,
+              selectedItemColor: MyColor.biru,
+              onTap: (index) {
+                widget.onItemTapped(index);
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
